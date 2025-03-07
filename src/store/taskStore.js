17451b1f72d3414e19819576b5daf63de4999b23
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2025-03-07 20:38:28 +0800

    feat: setup pinia store for tasks includes, add,mark completed, remove and setting of tasks from localStorge

---
+++ src/store/taskStore.js
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+import { getLocalStorage } from "../libs/utils";
+
+const useTaskStore = defineStore("tasks", {
+  state: () => ({
+    tasks: [],
+  }),
+  actions: {
+    addTask(task) {
+      const newTasks = {
+        id: Date.now(),
+        title: task,
+        completed: false,
+      };
+      this.tasks.push(newTasks);
+    },
+    markCompleted(id) {
+      this.tasks = this.tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    },
+    removeTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    setItem() {
+      const storedTasks = getLocalStorage("tasks");
+      this.tasks = storedTasks;
+    },
+  },
+});
+
+export default useTaskStore;
